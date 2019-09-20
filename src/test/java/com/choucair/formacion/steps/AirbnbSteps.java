51d037b6973273b68936a61813e7296e04b5b242
Mariale <malejadv93@gmail.com>
2019-09-20 10:56:24 -0500

    creacion proyecvto base

---
+++ src/test/java/com/choucair/formacion/steps/AirbnbSteps.java
@@ -0,0 +1,11 @@
+package com.choucair.formacion.steps;
+
+import com.choucair.formacion.pageobjects.AirbnbPage;
+import net.thucydides.core.annotations.Step;
+
+public class AirbnbSteps {
+    AirbnbPage airbnbPage;
+    @Step
+
+
+}
