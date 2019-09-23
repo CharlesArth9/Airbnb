9d56574fc1412246b68018ba4ee92158430e0ac0
cmestral <charlesarth@gmail.com>
2019-09-23 13:28:51 -0500

    actualizando

--- src/test/java/com/choucair/formacion/pageobjects/AirbnbPage.java
+++ src/test/java/com/choucair/formacion/pageobjects/AirbnbPage.java
@@ -10,9 +10,6 @@ import org.openqa.selenium.Point;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
-import java.awt.*;
-
-import static javax.swing.UIManager.getString;
 
 
 @DefaultUrl("https://cutt.ly/JwZ2rzv")
@@ -24,7 +21,6 @@ public class AirbnbPage extends PageObject {
         jse.executeScript("window.scrollBy(0, 463)");
         $("//button[contains(text(),'Ubicación')]").click();
 
-//        getDriver().findElement(By.tagName("html")).sendKeys(Keys.chord(Keys.CONTROL, Keys.ADD));
 //        $("//a[@class]//div[@class='_czm8crp'][contains(text(),'Pris')]").click();
 //        waitFor(5).second();
 //        getDriver().get("https://www.google.com/maps/@42.6613213,21.1656307,16z?hl=es");
@@ -33,18 +29,14 @@ public class AirbnbPage extends PageObject {
 
     public void moverMunequito()  {
         WebElement munequito = $("//div[@class='gm-svpc']");
-        new Actions(getDriver()).clickAndHold(munequito).moveByOffset(492,4659).release().perform();
+        Actions act=new Actions(getDriver());
+        act.dragAndDropBy(munequito,492, 4659).build().perform();
+//        new Actions(getDriver()).clickAndHold(munequito).moveByOffset(492,4659).release().perform();
 
 //        super.clickOn(munequito);
         waitFor(5).second();
-//        estoy probando nuevo alias para este commit de nuevo
 //        Point coor = getDriver().findElement(By.xpath("//body[@class='with-new-header']/div/div/main[@id='site-content']/div/section/div[@id='room']/div[@class='_mwt4r90']/div/div[@class='_1k6i3d4']/div[@class='_mkyacvg']/div/div[@id='neighborhood']/div[@class='_1gw6tte']/div/div/div/div[@class='_e296pg']/section/div/div/div/div/div/div/div[@class='gm-style']/div/div[3]")).getLocation();
 //        System.out.println(coor);
-//        Actions builder = new Actions(getDriver());
-//        builder.moveByOffset( 454, 4664 );
-
-        /*Robot robot = new Robot();
-        robot.mouseMove(454,4664);*/
     }
 
 }
